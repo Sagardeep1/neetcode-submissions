b0c43be96e5a8f795a06d5f57df49b4d1f744435
Sagardeep1 <51158719+Sagardeep1@users.noreply.github.com>
2026-08-25 16:05:04 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t factor = pow(2,31);
+        uint32_t ans = 0;
+        for(int i=0;i<32;i++) {
+            ans += factor * (n & 1);
+            n = n >> 1;
+            factor /= 2;
+        }
+        return ans;
+    }
+};
